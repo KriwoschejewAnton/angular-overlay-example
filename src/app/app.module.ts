3a1bf5d1ab82a85c3049715dcf2430ca06907e9e
KriwoschejewAnton <akrivoshey@wiley.com>
2022-08-31 16:11:50 +0300

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,14 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {OverlayModule} from '@angular/cdk/overlay';
 import {CdkOverlayBasicExample} from './cdk-overlay-basic-example';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatNativeDateModule} from '@angular/material/core';
-import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [CdkOverlayBasicExample],
   imports: [
-    BrowserAnimationsModule,
     BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    MatNativeDateModule,
     OverlayModule,
-    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [CdkOverlayBasicExample],
